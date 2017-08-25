3bf21ea9bb4955b570523498d53d7db8e6a332b2
Rafael Carlo André de Queiroz <rafaelqueiroz@msn.com>
2017-08-25 12:55:46 -0300

    Added some examples about threads.

---
+++ SimpleThreads.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_in_CSharp
+{
+    /// <summary>
+    /// Note that the method Main is also a Thread. So Print1 will be executed 
+    /// at the same time as the Main Thread.. so you can see some scrambled data 
+    /// on your console.
+    /// </summary>
+    class SimpleThreads
+    {
+        //static void Main(string[] args)
+        //{
+        //    Thread t = new Thread(Print1);
+
+        //    t.Start();
+
+        //    for (int i = 0; i < 1000; i++)
+        //    {
+        //        Console.Write(0);
+        //    }
+        //}
+
+        //public static void Print1()
+        //{
+        //    for (int i = 0; i < 1000; i++)
+        //    {
+        //        Console.Write(1);
+        //    }
+        //}
+    }
+}
---
+++ SleepingThreads.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threads_in_CSharp
+{
+    //static void Main(string[] args)
+    //{
+    //int num = 1;
+
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    Console.WriteLine(num);
+    //    Thread.Sleep(1000);
+    //    num++;
+    //}
+
+    //Console.WriteLine("Thread Ends");
+    //}
+}
