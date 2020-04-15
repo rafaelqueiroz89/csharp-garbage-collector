0d84391b234b9556b784477e905f69e8e5f4856d
rafaelqueiroz89 <rafaelqueiroz@msn.com>
2020-04-15 04:12:04 +0100

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_garbage_collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
